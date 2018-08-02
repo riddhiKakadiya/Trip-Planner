dc679439e24a577736bbd9df230c742467f97c20
preetamjain01 <jain.pree@husky.neu.edu>
2018-08-02 17:15:00 -0400

    Adding functionality for likes

---
+++ TravelMVC/TravelMVC/wwwroot/js/likes.js
@@ -0,0 +1,39 @@
+﻿//get user, reviews....then post user's like to that review
+function likeReview(obj) {
+    var review = {
+        description: obj.review.text,
+        dateOfReview: "2016-10-02"
+    };
+    var username = obj.user;
+    var url = 'http://localhost:8080/api/user/';
+
+    $.ajax({
+        url: url,
+        type: 'get',
+        data: { username: username },
+        success: function (user) {
+            url = 'http://localhost:8080/api/review/';
+            $.ajax({
+                url: url,
+                type: 'post',
+                contentType: "application/json; charset=utf-8",
+                data: JSON.stringify(review),
+                success: function (response) {
+                    var like = {
+                        user: user[0],
+                        review: response
+                    };
+                    url = 'http://localhost:8080/api/user/review/like';
+                    $.ajax({
+                        url: url,
+                        type: 'post',
+                        contentType: "application/json; charset=utf-8",
+                        data: JSON.stringify(like),
+                        success: function (like_response) {
+                        }
+                    });
+                }
+            });
+        }
+    });
+}
